f9638a84f3638e723cad74bee59785e006f68796
david <david@DESKTOP-20G0GN9>
2025-05-20 23:56:02 -0300

    Add project files.

---
+++ APICatalogo/Contexto/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Contexto
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+    }
+}
---
+++ APICatalogo/Domain/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Domain
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; }
+    }
+}
